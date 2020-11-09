b25f47a7515bf90f1a91356c53f9af9b5b822721
harukaty04 <haruka.kimura04@gmail.com>
2020-11-09 23:39:31 +0900

    edit controller.rb

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -1,7 +1,13 @@
-require 'test_helper'
 
 class TweetsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
 end
